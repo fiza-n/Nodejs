81528ecdf20e49311b672b88e0b8876df59bfa89
fiza noor <fnoor034726@gmail.com>
2026-07-18 00:14:20 +0500

    url format

--- server/index.js
+++ server/index.js
@@ -1,19 +1,27 @@
 import http from "http"
 import fs from "fs"
-
+import url from "url"
 
 
 const server = http.createServer((req, res)=>{
+    if(req.url === "/favicon.ico")return req.end;
     const log = `${Date.now() } ${req.url} New Request received\n`;
+    const myUrl = url.parse(req.url,true)
+    console.log(myUrl);
 
     fs.appendFile("log.txt", log, (err) =>{
         
-         switch(req.url){
+         switch(myUrl.pathname){
             case "/":
             res.end("Homepage");
             break;
             case "/about":
-            res.end("I am  Fiza Noor");
+            const username = myUrl.query.myname;
+            res.end(`Hi ${username}`);
+            break;
+            case "/search":
+            const search = myUrl.query.search_query;
+            res.end(`Search result for ${search}`);
             break;
             default:
             res.end(" 404 not found");
