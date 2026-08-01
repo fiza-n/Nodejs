f935d11507ebd4599a559ee24fcd6cee3fcf674a
fiza Noor <fnoor034726@gmail.com>
2026-08-01 23:58:50 +0500

    changes fixed

--- Instagram-Blog/app.js
+++ Instagram-Blog/app.js
@@ -1,4 +1,4 @@
-
+import 'dotenv/config'
 import express from "express"
 import path from "path"
 import userRoute from "./routes/user.js"
@@ -26,7 +26,7 @@ app.use(express.static(path.resolve("./public")))
 
 
 app.get("/", async (req,res)=>{
-  const allBlogs = await Blog.find({}).populate("createdBy", "fullname profileImageUrl")
+  const allBlogs = await Blog.find({})
     res.render("home", {
         user: req.user,
         blogs: allBlogs
@@ -37,4 +37,4 @@ app.use("/blog",blogRoute)
 
 app.listen(PORT, ()=>{
     console.log(`Server has started on Port ${PORT}`)
-})import 'dotenv/config'
+})
